49ab61db549765b57f70283c1410ede305f62266
Wiliam G <wiliam.gomes99@gmail.com>
2023-05-23 22:05:02 -0300

    Adiciona criptografia para as senhas no banco de dados

--- api/src/controllers/UserController.js
+++ api/src/controllers/UserController.js
@@ -1,11 +1,29 @@
 const User = require('../models/Users');
+const auth = require('./auth');
+const bcrypt = require('bcrypt');
 
 module.exports = {
     async store(req, res) {
         const { user_name, password } = req.body;
-
-        const user = await User.create({ user_name, password })
+        const salt = 12;
+        const passwordHash = await bcrypt.hash(password, salt);
+        const user = await User.create({ user_name, password: passwordHash });
 
         return res.json(user);
+     },
+
+     async log_in(req, res) {
+        const { user_name, password } = req.body;
+        const user = await User.findOne({ where: { user_name }});
+
+        // o password retornado por user.password é um hash
+        const match = await bcrypt.compare(password, user.password)
+
+        if(match) {
+         return res.send(user.user_name);
+        } else {
+         return res.send("Senha incorreta!");
+        }
+
      }
-}
+};
--- api/src/routes.js
+++ api/src/routes.js
@@ -4,5 +4,6 @@ const UserController = require('./controllers/UserController');
 const routes = express.Router();
 
 routes.post('/users', UserController.store);
+routes.post('/login', UserController.log_in);
 
 module.exports = routes;
